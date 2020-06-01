266507ca9ba750fd62e9b8b6765262a3e3abda0b
siam <siammohammad@iut-dhaka.edu>
2020-06-01 18:43:16 +0600

    contact-us page has done

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { db } from "../services/firebase";
 
 class ContactUs extends Component {
+  state = {
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+
+  contactUsHandler = () => {
+    const data = {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      message: this.state.message,
+    };
+    db.collection("contact-us")
+      .add(data)
+      .then((response) => {
+        console.log(response);
+      });
+  };
   render() {
     return (
       <section className="my-5 py-5">
@@ -30,7 +52,11 @@ class ContactUs extends Component {
                   <input
                     className="form-control"
                     type="text"
+                    value={this.state.name}
                     placeholder="Name"
+                    onChange={(event) =>
+                      this.setState({ name: event.target.value })
+                    }
                   ></input>
                 </div>
                 <div className="form-group">
@@ -38,6 +64,10 @@ class ContactUs extends Component {
                     className="form-control"
                     type="email"
                     placeholder="E-mail"
+                    value={this.state.email}
+                    onChange={(event) =>
+                      this.setState({ email: event.target.value })
+                    }
                   ></input>
                 </div>
                 <div className="form-group">
@@ -45,14 +75,30 @@ class ContactUs extends Component {
                     className="form-control"
                     type="tel"
                     placeholder="Phone"
+                    value={this.state.phone}
+                    onChange={(event) =>
+                      this.setState({ phone: event.target.value })
+                    }
                   ></input>
                 </div>
                 <textarea
                   className="form-control"
                   cols="30"
                   rows="3"
+                  value={this.state.message}
                   placeholder="Message"
+                  onChange={(event) =>
+                    this.setState({ message: event.target.value })
+                  }
                 />
+                <Link
+                  to="/"
+                  onClick={this.contactUsHandler}
+                  className="btn btn-outline-primary text-uppercase mt-2"
+                >
+                  <i className="fab fa-telegram-plane" />
+                  &nbsp;Send
+                </Link>
               </form>
             </div>
           </div>
